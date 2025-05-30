983b4b21f0c710a530fa0b481d5e165459c973e9
ZeonHK <71115874+ZeonHK@users.noreply.github.com>
2025-05-30 21:12:20 +0900

    Update test_car.py

--- test_car.py
+++ test_car.py
@@ -22,8 +22,8 @@ def test_initial_time(car):
 
 # Tests for changing speed
 def test_change_speed(car):
-    car.change_speed(1)
-    assert car.speed == 1
+    car.change_speed(7)
+    assert car.speed == 7
 
 
 def test_multiple_speed_change(car):
@@ -44,13 +44,3 @@ def test_get_current_speed_from_start(car):
 def test_get_current_speed_after_change(car):
     car.change_speed(27)
     assert car.get_current_speed() == 27
-
-def test_average_speed_div(car):
-    car.time = 0
-    assert car.average_speed == "Divide by 0! Car did not move!"
-    
-
-def test_average_speed_aver(car):
-    car.time = 10
-    car.change_speed(27)
-    assert car.average_speed == 2.7
